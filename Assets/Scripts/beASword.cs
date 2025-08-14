cc3ffe35976f38a6c769040f32222afbbe4e3c66
abbrev32 <kaungcho73@gmail.com>
2025-08-14 21:36:15 +0630

    ၆

--- Assets/Scripts/beASword.cs
+++ Assets/Scripts/beASword.cs
@@ -4,13 +4,15 @@ public class SwordMouseRotate : MonoBehaviour
 {
     public float rotationSpeed = 5f; // Adjust to your liking
     public Transform swordPivot; // The empty pivot object
-
+    float currentAngle = 0f;//rotation memory
     void Update()
     {
         // Get mouse horizontal movement
         float mouseX = Input.GetAxis("Mouse X");
 
+        currentAngle += mouseX * rotationSpeed;//rotation update
         // Rotate pivot based on mouse movement
-        swordPivot.Rotate(Vector3.forward, mouseX * rotationSpeed);
+        swordPivot.localRotation = Quaternion.Euler(0f,0f,currentAngle);
+        
     }
 }
