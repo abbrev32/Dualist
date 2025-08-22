ec97a40e2f3b22b7aa514a17f3681a23b87694a9
ArkarMoeThant <arkarmoethant05@gmail.com>
2025-08-22 22:37:43 +0630

    Switch

---
+++ Assets/Scripts/Elevator2.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using Mirror;
+
+public class Elevator2 : NetworkBehaviour
+{
+    [SerializeField] private Transform endPoint;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private float speed = 2f;
+
+    private bool activated = false;
+
+    [Server] // Only server should trigger this
+    public void ActivateElevator()
+    {
+        if (!activated)
+        {
+            activated = true;
+            RpcMoveElevator();
+        }
+    }
+
+    [ClientRpc]
+    private void RpcMoveElevator()
+    {
+        StartCoroutine(MoveElevatorRoutine());
+    }
+
+    private System.Collections.IEnumerator MoveElevatorRoutine()
+    {
+        Vector3 start = transform.position;
+
+        // Decide if we should go up or down
+        Vector3 target = (Vector3.Distance(start, startPoint.position) < Vector3.Distance(start, endPoint.position))
+                         ? endPoint.position : startPoint.position;
+
+        while (Vector3.Distance(transform.position, target) > 0.05f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = target;
+    }
+}
