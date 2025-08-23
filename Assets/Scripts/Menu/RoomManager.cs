e5200e30b8e5df6cc8c3a21141c4f8427d50204b
coke <sweetysky747@gmail.com>
2025-08-23 22:23:09 +0630

    LEE

--- Assets/Scripts/Menu/RoomManager.cs
+++ Assets/Scripts/Menu/RoomManager.cs
@@ -3,36 +3,15 @@ using Mirror;
 
 public class RoomManager : NetworkRoomManager
 {
-    [Header("UI Controller")]
-    public MainMenuController menuController;
+    public Transform spawnPoint;
 
     public override void OnRoomServerPlayersReady()
     {
-        // Only call the UI update function when the game is ready to start
-        // This is the correct callback for when all players are ready.
-        if (menuController != null)
+        if (allPlayersReady)
         {
-            menuController.UpdatePlayerListUI();
+            Debug.Log("All players ready. Starting Level1...");
+            ServerChangeScene("SampleScene"); // loads Level1
         }
     }
 
-    public override void OnRoomClientEnter()
-    {
-        base.OnRoomClientEnter();
-        // This is a good place to update the UI when a player first joins
-        if (menuController != null)
-        {
-            menuController.UpdatePlayerListUI();
-        }
-    }
-
-    public override void OnRoomClientExit()
-    {
-        base.OnRoomClientExit();
-        // This is a good place to update the UI when a player leaves
-        if (menuController != null)
-        {
-            menuController.UpdatePlayerListUI();
-        }
-    }
 }
