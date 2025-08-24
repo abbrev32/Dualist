ff6b7951fbcc21253167b030dc36ed3478584b41
coke <sweetysky747@gmail.com>
2025-08-24 23:02:07 +0630

    Final

--- Assets/Scripts/Enemy/Monster.cs
+++ Assets/Scripts/Enemy/Monster.cs
@@ -2,6 +2,7 @@ using Mirror;
 using UnityEngine;
 public class Monster : NetworkBehaviour
 {
+    [ServerCallback]
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!NetworkServer.active) return;
@@ -14,17 +15,17 @@ public class Monster : NetworkBehaviour
             }
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("Player"))
-        {
-            PlayerHealth playerHealth = collision.collider.GetComponent<PlayerHealth>();
-            if (playerHealth != null)
-            {
-                playerHealth.CmdTakeDamage(1);
-            }
-        }
-    }
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     if (collision.collider.CompareTag("Player"))
+    //     {
+    //         PlayerHealth playerHealth = collision.collider.GetComponent<PlayerHealth>();
+    //         if (playerHealth != null)
+    //         {
+    //             playerHealth.CmdTakeDamage(1);
+    //         }
+    //     }
+    // }
     [Server]
     public void Kill()
     {
