b848d7362454f867248a300b8d158ec62162e518
coke <sweetysky747@gmail.com>
2025-08-24 09:57:05 +0630

    PVPPVPPVPVPPV

--- Assets/Scripts/Player/SwordScript.cs
+++ Assets/Scripts/Player/SwordScript.cs
@@ -1,17 +1,23 @@
+using Mirror;
 using UnityEngine;
-public class SwordScript : MonoBehaviour
+public class SwordScript : NetworkBehaviour
 {
+    public static bool pvp = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!Mirror.NetworkServer.active) return;
-        //if (collision.CompareTag("Player"))
-        //{
-        //    PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
-        //    if (playerHealth != null)
-        //    {
-        //        playerHealth.TakeDamage(1);
-        //    }
-        //}
+        if (!isLocalPlayer && pvp)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+                if (playerHealth != null)
+                {
+                    playerHealth.TakeDamage(1);
+                }
+            }
+        }
         if (collision.CompareTag("Monster"))
         {
             Debug.Log("Monster Hit");
